cb1a7710c044ceb1e0e1525bc790e5ad4d414788
Poonam Kumari <poonam0502@gmail.com>
2020-07-08 20:10:53 +0530

    changes

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
-const path = require('path')
+const path = require('path');
 
 require('dotenv').config();
 
@@ -30,11 +30,11 @@ app.use('/users', usersRouter);
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
-  
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
 }
-
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
 });
-
